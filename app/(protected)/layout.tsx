d207c9859db9b58d1c1a97b274d9ece955b43a4a
Akshay Patel <akshaypatel.ap1320@gmail.com>
2024-11-28 23:06:14 +0530

    Suspense boundary added for working of useSearchParams, protected route layout changem

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -9,12 +9,12 @@ const ProtectedLayout = ({ children }: Props) => {
   return (
     <div
       className="h-full w-full flex flex-col gap-y-10 items-center 
-        justify-center bg-gradient-to-r from-indigo-500 to-emerald-500"
+        justify-center bg-gradient-to-r from-indigo-500 to-emerald-500 overflow-auto"
     >
-      <div className="flex-initial mt-2">
+      <div className="fixed mt-2 top-0">
         <Navbar />
       </div>
-      <div className="flex-1">{children}</div>{" "}
+      <div className="flex-1 mt-[100px] mb-10">{children}</div>{" "}
     </div>
   );
 };
