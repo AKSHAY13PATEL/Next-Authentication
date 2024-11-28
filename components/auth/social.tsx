caeff4a1e4cf56ee6977ffe2345ef111a12dd879
Akshay Patel <akshaypatel.ap1320@gmail.com>
2024-11-28 21:55:24 +0530

    Social log in , callback url set for redirection

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -3,11 +3,17 @@
 import { Button } from "@/components/ui/button";
 import { DEFAULT_LOGIN_REDIRECT } from "@/route";
 import { signIn } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 
 const Social = () => {
+  const params = useSearchParams();
+  const callbackUrl = params.get("callbackUrl");
+
   const onClick = async (provider: "google" | "github") => {
-    await signIn(provider, { callbackUrl: DEFAULT_LOGIN_REDIRECT });
+    await signIn(provider, {
+      callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT,
+    });
   };
   return (
     <div className="flex items-center w-full gap-x-2">
